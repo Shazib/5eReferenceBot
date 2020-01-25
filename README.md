53283d82e72833f1d48c002b2434c6ba38e7702e
Shazib Hussain <me@shazib.com>
2020-01-25 12:05:05 +0000

    Fixing for API Changes + Lambda support

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 5eReferenceBot
+
 A DND5e reference bot for Telegram
+
+Set up to run in AWS Lambda
+
+Initial code from https://github.com/shadsbot
--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Telegraf = require('telegraf');
 const axios = require("axios");
 const utf8 = require("utf8");
-const bot = new Telegraf("apikey"); // Enter in your API key here
+
+const bot = new Telegraf('api-token');
 
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
@@ -9,10 +10,9 @@ const Markup = require('telegraf/markup')
 bot.use((ctx, next) => {
     const start = new Date()
     return next(ctx).then(() => {
-      const ms = new Date() - start
-      console.log('Response time %sms: (%s) %s', ms, ctx.message.from.username, ctx.message.text )
+        const ms = new Date() - start
     })
-  })
+})
 
 function apisearch(apiurl) {
     return axios.get(apiurl)
@@ -22,36 +22,39 @@ function apigetitem(apiurl) {
 }
 
 function replySpell(ctx) {
-    var spell = ctx.message.text
-    spell = spell.replace(/\/spell /g,"")
-    spell = spell.replace(/\b\w/g, l => l.toUpperCase()) // Capitalize first letter of every word
-    keyboard = Markup.inlineKeyboard([
+
+    var spell = ctx.message.text;
+    spell = spell.replace(/\/spell /g, "");
+
+    var keyboard = Markup.inlineKeyboard([
         Markup.urlButton('📜 Read the full text', 'https://roll20.net/compendium/dnd5e/' + spell)
         // Markup.callbackButton('Delete', 'delete')
-      ])
-    spell = spell.replace(/ /g,"+")
-    apisearch(`http://dnd5eapi.co/api/spells/?name=${spell}`).then(function (response) {
-        apigetitem(response.data.results[0].url).then(function (responsedata) {
-            var msg = composeMessageSpell(responsedata.data)
-            ctx.replyWithMarkdown(msg, Extra.markup(keyboard))
-        });
+    ]);
+
+    spell = spell.replace(/ /g, "-");
+    spell = spell.toLowerCase();
+
+    apisearch(`http://dnd5eapi.co/api/spells/${spell}`).then(function (response) {
+        var msg = composeMessageSpell(response.data);
+        ctx.replyWithMarkdown(msg, Extra.markup(keyboard));
     });
 }
 
 function replyMonster(ctx) {
-    var monster = ctx.message.text
-    monster = monster.replace(/\/monster /g,"")
-    monster = monster.replace(/\b\w/g, l => l.toUpperCase()) // Capitalize first letter of every word
-    keyboard = Markup.inlineKeyboard([
+
+    var monster = ctx.message.text;
+    monster = monster.replace(/\/monster /g, "");
+
+    var keyboard = Markup.inlineKeyboard([
         Markup.urlButton('📜 Read the full text', 'https://roll20.net/compendium/dnd5e/' + monster)
-        // Markup.callbackButton('Delete', 'delete')
-      ])
-    monster = monster.replace(/ /g,"+")
-    apisearch(`http://dnd5eapi.co/api/monsters/?name=${monster}`).then(function (response) {
-        apigetitem(response.data.results[0].url).then(function (responsedata) {
-            var msg = composeMessageMonster(responsedata.data)
-            ctx.replyWithMarkdown(msg, Extra.markup(keyboard))
-        });
+    ]);
+
+    monster = monster.replace(/ /g, "-");
+    monster = monster.toLowerCase();
+
+    apisearch(`http://dnd5eapi.co/api/monsters/${monster}`).then(function (response) {
+        var msg = composeMessageMonster(response.data);
+        ctx.replyWithMarkdown(msg, Extra.markup(keyboard));
     });
 }
 
@@ -63,14 +66,20 @@ function composeMessageMonster(monster) {
     msg += `\n*AC*: ${monster.armor_class} *HP*: ${monster.hit_points} *HD*: ${monster.hit_dice}\n`
     msg += `*STR*: ${monster.strength} *DEX*: ${monster.dexterity} *CON*: ${monster.constitution} *INT*: ${monster.intelligence} *WIS*: ${monster.wisdom} *CHA*: ${monster.charisma}\n`
 
-    msg += `\n_Traits:_\n`
-    for (var trait of monster.special_abilities) {
-        msg += `*${trait.name}*: ${trait.desc}\n`
+    if (monster.special_abilities) {
+        msg += `\n_Traits:_\n`
+        for (var trait of monster.special_abilities) {
+            msg += `*${trait.name}*: ${trait.desc}\n`
+        }
     }
-    msg += `\n_Actions:_\n`
-    for (var trait of monster.actions) {
-        msg += `*${trait.name}*: ${trait.desc}\n`
+
+    if (monster.actions) {
+        msg += `\n_Actions:_\n`
+        for (var trait of monster.actions) {
+            msg += `*${trait.name}*: ${trait.desc}\n`
+        }
     }
+
     if (monster.damage_immunities || monster.damage_resistances || monster.damage_vulnerabilities || monster.condition_immunities) { msg += `\n` }
     if (monster.damage_vulnerabilities) { msg += `*Vulnerable* to ${monster.damage_vulnerabilities}\n` }
     if (monster.damage_resistances) { msg += `*Resistance* to ${monster.damage_resistances}\n` }
@@ -85,27 +94,37 @@ function composeMessageSpell(spell) {
     var spelldesc = spell.desc + ""
     var hl = spell.higher_level || "_This spell has no higher level function_";
     // Weird encoding errors?
-    spelldesc = spelldesc.replace(/â€™/g,"'");
-    spelldesc = spelldesc.replace(/â€�/g,"\"");
-    spelldesc = spelldesc.replace(/â€œa/g,"\"");
-    hl = hl+"".replace(/â€™/g,"'");
-    hl = hl+"".replace(/â€�/g,"\"");
-    hl = hl+"".replace(/â€œ/g,"\"");
+    spelldesc = spelldesc.replace(/â€™/g, "'");
+    spelldesc = spelldesc.replace(/â€�/g, "\"");
+    spelldesc = spelldesc.replace(/â€œa/g, "\"");
+    hl = hl + "".replace(/â€™/g, "'");
+    hl = hl + "".replace(/â€�/g, "\"");
+    hl = hl + "".replace(/â€œ/g, "\"");
 
     msg = `***${spell.name}*** R:${spell.range} ${spell.components}
 ${spell.duration}/${spell.casting_time}, Level ${spell.level} Spell
-
 ${spelldesc}
-
 ${hl}`
 
     return msg
 }
 
-// bot.start((ctx) => ctx.reply('Hey there!'))
+bot.start((ctx) => ctx.reply('Hey there!'))
 
-bot.command('spell', (ctx) => replySpell(ctx) )
-bot.command('monster', (ctx) => replyMonster(ctx) )
+bot.command('spell', (ctx) => replySpell(ctx))
+bot.command('monster', (ctx) => replyMonster(ctx))
 // bot.action('delete', ({ deleteMessage }) => deleteMessage())
 
-bot.startPolling()
+/* AWS Lambda handler function */
+exports.handler = (event, context, callback) => {
+    bot.handleUpdate(event); // make Telegraf process that data
+
+    return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+        statusCode: 200,
+        body: '',
+    });
+};
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
